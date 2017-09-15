0ab9b858d0a2bb88d1841bd98842ad25541117be
Thomas Gillam <tpgillam@googlemail.com>
2017-09-15 20:39:58 +0100

    Improve test running

---
+++ scripts/run_tests.sh
@@ -0,0 +1,4 @@
+echo "Running all tests..."
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. $SCRIPTS_DIR/setup.sh
+nosetests pypuffin --with-doctest
---
+++ scripts/setup.sh
@@ -0,0 +1,3 @@
+. ~/scripts/anaconda_setup.sh
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export PYTHONPATH=$SCRIPTS_DIR/../:$PYTHONPATH
