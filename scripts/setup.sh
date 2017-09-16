54e045779112f35e531075e0295873286234c723
Thomas Gillam <tpgillam@googlemail.com>
2017-09-16 11:13:43 +0100

    Only add to PYTHONPATH if required

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,7 @@
 . ~/scripts/anaconda_setup.sh
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export PYTHONPATH=$SCRIPTS_DIR/../:$PYTHONPATH
+# Only add the path if it isn't already on the python path
+if [[ $PYTHONPATH != *"pypuffin"* ]];
+then
+    export PYTHONPATH=$SCRIPTS_DIR/../:$PYTHONPATH
+fi
