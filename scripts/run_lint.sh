da921f42b15f314eb5a3a1549139d2bf6f408d7d
Thomas Gillam <tpgillam@googlemail.com>
2017-09-19 22:29:28 +0100

    Add support for mypy static type checker

--- scripts/run_lint.sh
+++ scripts/run_lint.sh
@@ -3,3 +3,12 @@ echo
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $SCRIPTS_DIR/setup.sh
 pylint --rcfile=$SCRIPTS_DIR/../pylintrc pypuffin
+
+echo "...done."
+echo
+
+# Static type checker
+echo "Running mypy..."
+echo
+mypy --ignore-missing-imports -p pypuffin
+echo "...done."
