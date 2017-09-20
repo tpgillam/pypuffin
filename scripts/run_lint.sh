305e25a13883327e56b3ad702ee1ffd786aca2fa
Thomas Gillam <tpgillam@googlemail.com>
2017-09-20 08:17:56 +0100

    mypy + numpy isn't so great right now

--- scripts/run_lint.sh
+++ scripts/run_lint.sh
@@ -8,7 +8,10 @@ echo "...done."
 echo
 
 # Static type checker
-echo "Running mypy..."
-echo
-mypy --ignore-missing-imports -p pypuffin
-echo "...done."
+# TODO - after some initial testing it seems like this isn't really quite ready for general use. It
+# pretty much doesn't work at all with numpy/scipy/sklearn. If & when that changes it might be worth reconsidering
+
+# echo "Running mypy..."
+# echo
+# mypy --ignore-missing-imports -p pypuffin
+# echo "...done."
