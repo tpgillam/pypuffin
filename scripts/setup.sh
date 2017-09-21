56b8683f169ac9ee76088b670f7c6bfbdeebf516
Thomas Gillam <tpgillam@googlemail.com>
2017-09-21 20:56:36 +0100

    Use tensorflow backend for Keras

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,6 @@
 . ~/scripts/anaconda_setup.sh
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export KERAS_BACKEND=tensorflow
 # Only add the path if it isn't already on the python path
 if [[ $PYTHONPATH != *"pypuffin"* ]];
 then
