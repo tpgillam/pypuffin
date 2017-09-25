99f90837812a6542989c8df62ec6e7715b583a4d
Thomas Gillam <tpgillam@googlemail.com>
2017-09-26 00:07:26 +0100

    theano + GPU not working

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,6 @@
 . ~/scripts/anaconda_setup.sh
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export THEANO_FLAGS='device=cpu,force_device=True'
 export KERAS_BACKEND=tensorflow
 # Only add the path if it isn't already on the python path
 if [[ $PYTHONPATH != *"pypuffin"* ]];
